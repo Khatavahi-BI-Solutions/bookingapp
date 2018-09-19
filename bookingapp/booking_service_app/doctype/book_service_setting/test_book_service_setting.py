d4e9032df9b08a84f2feb74ed764297d4bb5cc64
Jigar Tarpara <jigartarpara68@gmail.com>
2018-09-19 14:38:59 +0530

    Added book Service Setting Doctype

---
+++ bookingapp/booking_service_app/doctype/book_service_setting/test_book_service_setting.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Jigar Tarpara and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestBookServiceSetting(unittest.TestCase):
+	pass
