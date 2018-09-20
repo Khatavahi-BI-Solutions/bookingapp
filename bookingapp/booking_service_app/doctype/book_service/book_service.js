83b5b4ca9987bcef633b04da2330c300b9d1a090
Jigar Tarpara <jigartarpara68@gmail.com>
2018-09-20 13:01:00 +0530

    Item Table add Image Field and qty and uom

--- bookingapp/booking_service_app/doctype/book_service/book_service.js
+++ bookingapp/booking_service_app/doctype/book_service/book_service.js
@@ -28,5 +28,32 @@ frappe.ui.form.on('Book Service Item', {
 			"child": child,
 			"fieldname": "service_item"
 		})
+		cur_frm.call({
+			"method": "frappe.client.get_value",
+			"args": {
+				"doctype": "Item",
+				"filters": {
+					"name":  child.item
+				},
+				"fieldname": "image"
+			},
+			"child": child,
+			"fieldname": "image"
+		})
+		cur_frm.call({
+			"method": "frappe.client.get_value",
+			"args": {
+				"doctype": "Item",
+				"filters": {
+					"name":  child.service_item
+				},
+				"fieldname": "stock_uom"
+			},
+			"child": child,
+			"fieldname": "stock_uom"
+		})
+		// frm.add_fetch("item", "image", "image");
+		// frm.add_fetch("item", "service_item", "service_item");
+		// frm.add_fetch("service_item", "stock_uom", "stock_uom");
 	}
 })
