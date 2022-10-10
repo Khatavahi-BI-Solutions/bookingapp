46b5918e38aa46093309cd9f0433901f2b1aa25c
Jigar Tarpara <jigartarpara68@gmail.com>
2022-10-10 10:51:57 +0530

    Validation added for Service Item

--- bookingapp/public/js/bookingapp_item.js
+++ bookingapp/public/js/bookingapp_item.js
@@ -3,5 +3,15 @@ frappe.ui.form.on('Item', {
         if(frm.doc.booking_item == 0){
             frm.doc.service_item = ""
         }
+    },
+    onload: function(frm){
+        frm.set_query("service_item", function() {
+            return {
+                "filters": {
+                    "is_service_item": true,
+                    "is_stock_item": false
+                }
+            };
+        });
     }
 })
--- bookingapp/utils.py
+++ bookingapp/utils.py
@@ -6,6 +6,7 @@ import frappe
 def make_booking_service_item(doc, method):
     if doc.booking_item == 1:
         create_service_item(doc)
+        validate_service_item(doc)
 
 
 def create_service_item(item):
@@ -17,12 +18,22 @@ def create_service_item(item):
             "item_name": item.item_name,
             "item_group": book_service_setting.service_item_group,
             "stock_uom": book_service_setting.default_unit_of_measure,
-            "is_stock_item": 0
+            "is_stock_item": False,
+            "is_service_item": True
         })
         service_item.save(ignore_permissions=True)
         item.service_item = service_item.name
     else:
         item.service_item = item.item_code + "_service"
         print("item_name",item.item_name + " (B) ")
-        frappe.db.set_value("Item",item.item_code + "_service", "item_name",item.item_name + " (B) ")
-        frappe.db.set_value("Item",item.item_code + "_service", "image",item.image)
+        frappe.db.set_value("Item",item.service_item, "item_name",item.item_name + " (B) ")
+        frappe.db.set_value("Item",item.service_item, "image",item.image)
+
+def validate_service_item(doc):
+    if doc.service_item:
+        is_service_item = frappe.db.get_value("Item",doc.service_item,"is_service_item")
+        if not is_service_item:
+            frappe.throw("Service Item must be checked Is Service Item.")
+        is_stock_item = frappe.db.get_value("Item",doc.service_item,"is_stock_item")
+        if is_stock_item:
+            frappe.throw("Stock Item can't be use as service item.")
