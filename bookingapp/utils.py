964a0b0fe1012ff145a305e400cf1eea67d20aab
Jigar Tarpara <jigartarpara68@gmail.com>
2022-10-10 10:26:06 +0530

    Update Service item on update of main item [Image and Item Name]

--- bookingapp/utils.py
+++ bookingapp/utils.py
@@ -4,16 +4,17 @@ import frappe
 
 
 def make_booking_service_item(doc, method):
-    if doc.booking_item == 1 and not doc.service_item:
-        book_service_setting = frappe.get_cached_doc("Khatavahi Book Service Setting")
-        create_service_item(doc, book_service_setting)
+    if doc.booking_item == 1:
+        create_service_item(doc)
 
 
-def create_service_item(item, book_service_setting):
+def create_service_item(item):
     if not frappe.db.exists("Item", item.item_code + "_service"):
+        book_service_setting = frappe.get_cached_doc("Khatavahi Book Service Setting")
         service_item = frappe.new_doc("Item")
         service_item.update({
             "item_code": item.item_code + "_service",
+            "item_name": item.item_name,
             "item_group": book_service_setting.service_item_group,
             "stock_uom": book_service_setting.default_unit_of_measure,
             "is_stock_item": 0
@@ -22,3 +23,6 @@ def create_service_item(item, book_service_setting):
         item.service_item = service_item.name
     else:
         item.service_item = item.item_code + "_service"
+        print("item_name",item.item_name + " (B) ")
+        frappe.db.set_value("Item",item.item_code + "_service", "item_name",item.item_name + " (B) ")
+        frappe.db.set_value("Item",item.item_code + "_service", "image",item.image)
